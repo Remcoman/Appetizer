b9d127afec8c7bf39f773b8fc28b707a4783520c
Remco Krams <info@remcokrams.nl>
2012-07-19 15:13:55 +0200

    Another major change to project layout

--- README.md
+++ README.md
@@ -1,23 +1,47 @@
 Very simple webapp framework.
 ====
 
-Step 1: development
+Step 1: installation
 ====
-
 1. Install nodejs
-2. Start the server (./start.sh)
-3. Put your Less style in src/styles/index.less
-4. Put your JavaScript in src/styles/index.js
-5. Visit http://localhost:8000.
-6. That's all
+2. Clone this repository to a central location (for example /appetizer)
+3. Source the appetizer.sh in your bash shell:
 
-Step 2: deployment
+	. /appetizer/appetizer.sh
+
+Step 2: development
 ====
-1. Execute the build script (./build.sh)
-2. Your code is in the build folder (CSS, JavaScript is minified)
-3. That's all
+1. Open terminal
+2. cd to your project directory
+3. Create your project:
+
+	appetizer make
+
+4. Start the development server:
+
+	appetizer start
+
+5. Put your Less style in src/styles/index.less
+6. Put your JavaScript in src/scripts/index.js
+7. Visit http://localhost:8000.
+8. That's all
+
+Step 3: deployment
+====
+1. Open terminal
+2. cd to your project directory
+3. Build it:
+
+	appetizer build
+
+4. Your code is in the build folder (CSS, JavaScript is minified)
+5. That's all
 
 Why is there only one Less and JavaScript file?
 ====
 index.less and index.js are your entry files. For Less you can use @import statements to include other less files.
 For JavaScript you can use the CommonJS module format to 'require' other js files.
+
+TODO
+====
+Add support for a template language like Jade?
--- appetizer.sh
+++ appetizer.sh
@@ -1,50 +1,84 @@
 #!/bin/bash
 
-TARGETDIR="$2"
-if [ "$TARGETDIR" ]; then
-	cd $TARGETDIR
-else
-	TARGETDIR=`pwd`
+#This file is heavily inspired by nvm.sh at https://github.com/creationix/nvm/blob/master/nvm.sh
+
+#directory of this file
+if [ ! -d "$APPETIZER_DIR" ]; then
+	export APPETIZER_DIR=`dirname "${BASH_SOURCE[0]}"`
 fi
 
-MYDIR=`dirname "$0"`
+appetizer()
+{
+	#the action
+    ACTION="$1"
+    if [ ! "$ACTION" ]; then
+    	ACTION="help"
+    fi
 
-ACTION="$1"
-if [ ! "$ACTION" ]; then
-	ACTION="help"
-fi
+	#the target directory to start all actions in
+	TARGET_DIR="$2"
+	if [ "$TARGET_DIR" ]; then
+		cd $TARGET_DIR
+	else
+		TARGET_DIR=`pwd`
+	fi
 
-case $ACTION in
-	"build" )
-		if [! -d "$TARGETDIR/build"]; then
-		    mkdir "$TARGETDIR/build"
-		fi
-		rm -r "$TARGETDIR/build/*"
-		cp -r "$TARGETDIR/src/*" "$TARGETDIR/build"
-		node "$MYDIR/lib/build.js" "$TARGETDIR"
-	;;
+	case $ACTION in
+		"build" )
+			if [ ! -d "$TARGET_DIR/build" ]; then
+			    mkdir "$TARGET_DIR/build"
+			fi
+			rm -r "$TARGET_DIR/build"
+			mkdir "$TARGET_DIR/build"
+			node "$APPETIZER_DIR/lib/build.js" "$TARGET_DIR"
+		;;
 
-	"start" )
-		node "$MYDIR/lib/server.js" start "$TARGETDIR/src"
-	;;
+		"start" )
+			node "$APPETIZER_DIR/lib/server.js" start "$TARGET_DIR"
+		;;
 
-	"stop" )
-		node "$MYDIR/lib/server.js" stop "$TARGETDIR/src"
-	;;
-	
-	"create" )
-		cp "$MYDIR/config.json" "$TARGETDIR/"
-		mkdir "$TARGETDIR/src"
-		mkdir "$TARGETDIR/build"
-	;;
-	
-	"help" )
-		echo
-		echo "Appetizer"
-		echo
-		echo "Usage:"
-		echo "	appetizer start			Start the development server"
-		echo "	appetizer stop			Stop the development server"
-		echo "	appetizer build			Build and minify all js and less files"
-	;;
-esac
+		"stop" )
+			node "$APPETIZER_DIR/lib/server.js" stop "$TARGET_DIR"
+		;;
+
+		"make" )
+			if [ "$(ls $TARGET_DIR)" ]; then
+				while true; do
+                    read -p "Directory not emty. Do you really want to make here? " yn
+                    case $yn in
+                        [Yy]* ) break;;
+                        [Nn]* ) return;;
+                        * ) echo "Please answer yes or no.";;
+                    esac
+                done
+			fi
+
+			cp "$APPETIZER_DIR/config.json" "$TARGET_DIR"
+
+			if [ ! -d "$TARGET_DIR/src" ]; then
+				mkdir "$TARGET_DIR/src"
+			fi
+
+			cp -r "$APPETIZER_DIR"/src/* "$TARGET_DIR/src"
+
+			if [ ! -d "$TARGET_DIR/build" ]; then
+				mkdir "$TARGET_DIR/build"
+			fi
+		;;
+
+		"help" )
+			echo
+			echo "Appetizer"
+			echo
+			echo "Usage:"
+			echo "	appetizer make			Creates a project according to a template"
+			echo "	appetizer start			Start the development server"
+			echo "	appetizer stop			Stop the development server"
+			echo "	appetizer build			Build and minify all js and less files"
+		;;
+
+		* )
+			appetizer help
+		;;
+	esac
+}
