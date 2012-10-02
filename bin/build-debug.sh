a64c6a2841a755dc68ad1d59bd021ca13c0b88d3
Remco Krams <info@remcokrams.nl>
2012-10-02 13:27:15 +0200

    Added ejs template plugin and removed the pathvars plugin (does not work with browserify)

--- bin/build-debug.sh
+++ bin/build-debug.sh
@@ -2,5 +2,3 @@
 MYDIR=`cd \`dirname "$0"\`; pwd`
 . $MYDIR/../appetizer.sh
 appetizer build-debug "$1"
-
-echo "Project build in $1"
--- bin/build-release.sh
+++ bin/build-release.sh
@@ -2,5 +2,3 @@
 MYDIR=`cd \`dirname "$0"\`; pwd`
 . $MYDIR/../appetizer.sh
 appetizer build-release "$1"
-
-echo "Project build in $1"
